8b23273f074c7518b53ec24f458de5ed68331715
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-20 18:12:22 +0300

    add django rest_framework

--- award/settings.py
+++ award/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'awarded',
     'cloudinary',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- awarded/urls.py
+++ awarded/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('',views.index,name='Index'),
+      url(r'^$',views.index,name='Index'),
     
   
 ]
