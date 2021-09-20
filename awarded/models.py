0699e29706bc27c091e496bb530acc07caa8f092
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-20 18:57:21 +0300

    add new html files

--- awarded/models.py
+++ awarded/models.py
@@ -8,3 +8,16 @@ class photos(models.Model):
     title = models.CharField(max_length=100)
     #image field
     image = CloudinaryField('image')
+    
+class categories(models.Model):
+    categories= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.categories
+
+    def save_category(self):
+        self.save()
+
+    @classmethod
+    def delete_category(cls,categories):
+        cls.objects.filter(categories=categories).delete()
