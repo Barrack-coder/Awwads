959eaa055f85ac6a298d0971d6e081a7724a8cc5
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-20 16:55:05 +0300

    modified base and idex.html

--- awarded/views.py
+++ awarded/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import request
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+def index(request):
+	return render(request,'index.html',)
